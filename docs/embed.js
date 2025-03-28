5b94f2dfe7ae751d504b503c4ef0ccf5fd9e8eeb
fOuttaMyPaint <TMhospitalitystrategies@gmail.com>
2025-03-28 16:54:09 -0400

    Refactor embed.js to enhance iframe security by removing 'allow-same-origin' from sandbox attributes. Update index.html footer for improved layout and accessibility, adding links for GitHub repo, issues, and license. Revamp styles.css for a cleaner footer design with better responsiveness and dark mode support.

--- docs/embed.js
+++ docs/embed.js
@@ -20,15 +20,15 @@ function embedElement(type, containerId) {
     switch (type) {
         case 'map':
             element.src = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d387193.30596073366!2d-74.25987701089373!3d40.69766998831435!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew%20York%2C%20NY!5e0!3m2!1sen!2sus!4v1614118575811!5m2!1sen!2sus';
-            element.setAttribute('sandbox', 'allow-scripts allow-same-origin');
+            element.setAttribute('sandbox', 'allow-scripts');
             break;
         case 'chart':
             element.src = 'https://charts.mongodb.com/charts-project-0-qwerty/embed/charts?id=62e9a6c1-ce35-4a97-8a3c-b9bad0e3dd1e&maxDataAge=3600&theme=light&autoRefresh=true';
-            element.setAttribute('sandbox', 'allow-scripts allow-same-origin allow-popups');
+            element.setAttribute('sandbox', 'allow-scripts allow-popups');
             break;
         case 'youtube':
             element.src = 'https://www.youtube.com/embed/dQw4w9WgXcQ';
-            element.setAttribute('sandbox', 'allow-scripts allow-same-origin allow-popups');
+            element.setAttribute('sandbox', 'allow-scripts allow-popups');
             break;
         default:
             container.innerHTML = '<p class="error">Unsupported embed type</p>';
